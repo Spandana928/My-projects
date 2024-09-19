ac7959d619aa897fea7c0c21611632d5d5fd39e0
Spandana928 <spandu0813@gmail.com>
2024-09-19 14:54:13 +0530

    Add files via upload

---
+++ temperature conversion/Temperature Conversion/temp2.js
@@ -0,0 +1,12 @@
+// script.js
+function convertToFahrenheit() {
+    const celsius = document.getElementById("celsius").value;
+    const fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit.toFixed(1);
+}
+
+function convertToCelsius() {
+    const fahrenheit = document.getElementById("fahrenheit").value;
+    const celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("celsius").value = celsius.toFixed(1);
+}
